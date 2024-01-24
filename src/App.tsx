4b368fcf1351ab353cd41002f519ca0efa0cb4a4
Baicov Andrei <andrey.baikov@indriver.com>
2024-01-24 12:08:58 +0200

    feat: init test request to backend. proxy enabled

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    try {
+
+      fetch(`/api/v1/app-service/get-apps`,
+        {
+          method: 'PUT',
+          body: JSON.stringify({
+            "pageNumber": 0,
+            "pageSize": 25
+          }),
+          headers: {
+            'Content-Type': 'application/json; charset=utf-8',
+            'ngrok-skip-browser-warning': '69420'
+          }
+        },
+      ).then((res) => {
+        console.log('status', res);
+        return res.text();
+      }).then(console.log)
+    } catch (error) {
+      console.error('🚀 ~ error:', error);
+
+    }
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
